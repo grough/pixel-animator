e8e2d6759568110a5bde1cc97ae3efa3d290f852
Gavin Rough <gavinrough@gmail.com>
2019-09-01 12:18:31 -0400

    Add optional scheduler animation paramter

--- src/render-web.ts
+++ src/render-web.ts
@@ -1,47 +1,29 @@
 import { animator, Animation, UserAnimation } from "./animator";
 import { baseAnimation } from "./defaults";
+import { createIntervalScheduler } from "./interval-scheduler";
 
 export function render(
   userAnimation: UserAnimation,
   canvas: HTMLCanvasElement,
+  userScheduler?: any,
 ) {
-  let playing = true;
   let imageData;
-  let timeoutId: number;
   const animation: Animation = { ...baseAnimation, ...userAnimation };
-  const interval = 1000 / animation.frameRate;
   canvas.width = animation.columns;
   canvas.height = animation.rows;
   const context = canvas.getContext("2d");
+  const renderScheduler =
+    userScheduler || createIntervalScheduler(1000 / animation.frameRate);
   let frameGenerator = animator(animation);
 
-  function paint() {
+  function renderNext() {
     const frame = frameGenerator.next();
     if (frame.done || !context) return;
     imageData = new ImageData(frame.value, animation.columns, animation.rows);
     context.putImageData(imageData, 0, 0);
-    if (playing) {
-      // @ts-ignore FIXME "Type NodeJS.Timeout is not assignable to number"
-      timeoutId = setTimeout(() => requestAnimationFrame(paint), interval);
-    }
   }
 
-  // Perform initial paint
-  if (playing) paint();
-
-  return {
-    play: () => {
-      playing = true;
-      paint();
-    },
-    pause: () => {
-      clearTimeout(timeoutId);
-      playing = false;
-    },
-    next: () => {
-      playing = false;
-      paint();
-    },
-    playing: () => playing,
-  };
+  const scheduler = renderScheduler(renderNext);
+  scheduler.play();
+  return scheduler;
 }
